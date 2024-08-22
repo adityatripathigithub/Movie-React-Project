1799f8a1e134a0ac5be48c18c34c2f933cec0d05
adity tripathi <tripathiaditya163@gmail.com>
2024-08-22 22:10:02 +0530

    Create Topnave

--- src/components/partials/Topnav.jsx
+++ src/components/partials/Topnav.jsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Topnav = () => {
+
+    const [query, setquery] = useState("");
+
+    console.log(query);
+
     return (
         <div className='w-full h-[10vh] relative flex justify-center items-center'>
 
-            <i class="text-3xl text-zinc-400 ri-search-2-line "></i>
-            
-            <input className='w-[60%] text-zinc-400 mx-10 p-5 text-xl outline-none  bg-transparent' type="text" placeholder='Scearch Anythings' />
-            
-            <i class="text-zinc-400 text-3xl ri-close-fill"></i>
+            <i className="text-3xl text-zinc-400 ri-search-2-line "></i>
+
+            <input
+                onChange={(e) => setquery(e.target.value)}
+                value={query}
+                className="w-[60%] text-zinc-400 mx-10 p-5 text-xl outline-none  bg-transparent" type="text"
+                placeholder='Scearch Anythings'
+            />
+
+            <i className="text-zinc-400 text-3xl ri-close-fill"></i>
+
+            <div className='absolute w-[50%] h-[50vh] bg-zinc-200 top-[90%] overflow-auto'>
+
+                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>hello everyone</span>
+                </Link>
+                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>hello everyone</span>
+                </Link>
+                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>hello everyone</span>
+                </Link>
+
+                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>hello everyone</span>
+                </Link>
+                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>hello everyone</span>
+                </Link>
+                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                    <img src="" alt="" />
+                    <span>hello everyone</span>
+                </Link>
+
+            </div>
         </div>
     )
 }
 
-export default Topnav
+export default Topnav 
