3d005aa84cd99eebed6627c62bcf98025880c723
adity tripathi <tripathiaditya163@gmail.com>
2024-08-24 22:14:00 +0530

    Create Top Nave

--- src/components/partials/Topnav.jsx
+++ src/components/partials/Topnav.jsx
@@ -8,7 +8,7 @@ const Topnav = () => {
     console.log(query);
 
     return (
-        <div className='w-full h-[10vh] relative flex justify-center items-center'>
+        <div className='w-full h-[10vh] relative flex justify-start ml-[15%] items-center'>
 
             <i className="text-3xl text-zinc-400 ri-search-2-line "></i>
 
@@ -18,37 +18,19 @@ const Topnav = () => {
                 className="w-[60%] text-zinc-400 mx-10 p-5 text-xl outline-none  bg-transparent" type="text"
                 placeholder='Scearch Anythings'
             />
+            {query.length > 0 &&
+                <i onClick={()=> setquery("")}
+                 className="cursor-pointer text-zinc-400 text-3xl ri-close-fill"></i>
+            }
 
-            <i className="text-zinc-400 text-3xl ri-close-fill"></i>
 
-            <div className='absolute w-[50%] h-[50vh] bg-zinc-200 top-[90%] overflow-auto'>
+            <div className='absolute w-[50%] max-h-[50vh] bg-zinc-200 top-[90%] overflow-auto rounded'>
 
-                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
+                {/* <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
                     <img src="" alt="" />
                     <span>hello everyone</span>
-                </Link>
-                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
-                    <img src="" alt="" />
-                    <span>hello everyone</span>
-                </Link>
-                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
-                    <img src="" alt="" />
-                    <span>hello everyone</span>
-                </Link>
-
-                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
-                    <img src="" alt="" />
-                    <span>hello everyone</span>
-                </Link>
-                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
-                    <img src="" alt="" />
-                    <span>hello everyone</span>
-                </Link>
-                <Link className='hover:text-black hover:bg-zinc-300 duration-300 text-zinc-600 font-semibold w-[100%] p-10 flex justify-stert items-center border-b-2 border-zinc-100'>
-                    <img src="" alt="" />
-                    <span>hello everyone</span>
-                </Link>
-
+                </Link> */}
+                
             </div>
         </div>
     )
