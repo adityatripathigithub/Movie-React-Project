5bc242a053526ebf78da9fe687f5757249945f85
adity tripathi <tripathiaditya163@gmail.com>
2024-09-20 23:56:34 +0530

    create personAction.jsx

--- src/components/PersonDetails.jsx
+++ src/components/PersonDetails.jsx
@@ -1,27 +1,25 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { asynceloadeperson, removeperson } from "../store/actions/personActions";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { asynceloadeperson, removeperson } from "../store/actions/personAction";
+import { Link, Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 import Loding from "./Loding";
 
 const PersonDetails = () => {
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const { info } = useSelector((state) => state.person);
+    const dispatch = useDispatch();
+    console.log(info);
 
-    const persondetails = () => {
-        const { pathname } = useLocation();
-        const navigate = useNavigate();
-        const { id } = useParams();
-        const { info } = useSelector((state) => state.person);
-        const dispatch = useDispatch();
-        console.log(info);
+    useEffect(() => {
+        dispatch(asynceloadeperson(id));
+        return () => {
+            dispatch(removeperson());
+        };
+    }, [id]);
 
-        useEffect(() => {
-            dispatch(asynceloadeperson(id));
-            return () => {
-                dispatch(removeperson());
-            };
-        }, [id]);
-
-        return info ? <div>PersonDetails</div>:<Loding />;
-    };
+    return info ? <div>PersonDetails</div>:<Loding />;
 };
+
 export default PersonDetails;
---
+++ src/store/actions/personAction.jsx
@@ -0,0 +1,25 @@
+export { removeperson } from "../redusers/personSlice";
+import axios from "../../utils/axios";
+import { loadperson } from "../redusers/personSlice";
+
+export const asynceloadeperson = (id) => async (dispatch, getstate) => {
+    try {
+        const detail = await axios.get(`/person/${id}`);
+        const externalid = await axios.get(`/person/${id}/external_ids`);
+        const combinedCredits = await axios.get(`/person/${id}/combined_credits`);
+        const tvCredits = await axios.get(`/person/${id}/tv_credits`);
+        const movieCredits = await axios.get(`/person/${id}/movie_credits`);
+        let theultimatedetails = {
+            detail: detail.data,
+            externalid: externalid.data,
+            combinedCredits: combinedCredits.data,
+            movieCredits: movieCredits.data,
+            tvCredits: tvCredits.data,
+        };
+
+        dispatch(loadperson(theultimatedetails));
+        console.log(theultimatedetails);
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+};
